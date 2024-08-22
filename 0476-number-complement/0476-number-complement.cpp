b728bd87e780f624367f4f30b0a823f656898858
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-08-23 03:19:28 +0530

    Time: 2 ms (50.2%), Space: 7.4 MB (58.43%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int findComplement(int num) {
-        if (num == 0) return 1; // Special case for 0, whose complement is 1
-        
-        unsigned int mask = ~0; // Start with a mask of all 1s (e.g., 111...111)
+        long answer = 0;
+        long pow = 1;
 
-        // Shift the mask left until it's just bigger than the number
-        while (num & mask) {
-            mask <<= 1;
+        while(num){
+            answer+=(num%2==0) ? pow :0;
+            num/=2;
+            pow*=2;
         }
-
-        // XOR the number with the inverse of the mask to get the complement
-        return ~mask & ~num;
-        
+        return answer;
     }
 };
