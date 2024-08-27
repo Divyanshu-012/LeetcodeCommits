8f1b74d2f3d14754a3501abf361ce73801d2570b
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-08-27 15:51:15 +0530

    Time: 0 ms (100%), Space: 7.8 MB (84.44%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> output;
-        vector<int> currentSeq;
-        vector<int> nums(9);
-        iota(nums.begin(), nums.end(), 1);
 
-        combination(nums, 0, n, k, currentSeq, output);
-
-
-        return output;
-    }
-
-private:
     bool combination(vector<int>& nums, int pos, int target, int length, vector<int>& currentSeq, vector<vector<int>>& output) {
         if(currentSeq.size() > length) {
             return false;
@@ -38,5 +26,19 @@ private:
         }
         return true;
     }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> output;
+        vector<int> currentSeq;
+        vector<int> nums(9);
+        iota(nums.begin(), nums.end(), 1);
+
+        combination(nums, 0, n, k, currentSeq, output);
+
+
+        return output;
+    }
+
+
+    
 
 };
