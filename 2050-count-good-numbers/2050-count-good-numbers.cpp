34c3f4ab955834ba776969da4306d75c5e6cd093
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-08-27 11:40:43 +0530

    Time: 0 ms (100%), Space: 7.4 MB (69.81%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -6,11 +6,14 @@ public:
     long long modPow(long long base, long long exp) {
         long long result = 1;
         while (exp > 0) {
+            
+            
             if (exp % 2 == 1) { 
                 result = (result * base) % mod;  // If exp is odd, multiply the base with the result
             }
             base = (base * base) % mod;  // Square the base
             exp /= 2;  // Divide the exponent by 2
+            
         }
         return result;
     }
