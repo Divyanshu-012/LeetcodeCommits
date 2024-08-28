e3d8d11ca63c2deffb382acda712bf4849bd6c5f
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-08-28 15:17:39 +0530

    Time: 7 ms (47.81%), Space: 16.1 MB (76.22%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,52 +1,10 @@
 class Solution {
 public:
 
-//   v  firstOcc(vector<int>&nums,int x){
-      
-//     }
 
 
-//    int  lastOcc(vector<int>&nums,int x){
-//         int n = nums.size();
-//         int low = 0;
-//         int high = n - 1;
-
-//         while (low <= high) {
-//             int mid = low + (high - low) / 2; 
-
-//             if (nums[mid] == x) {
-//                 int  i = mid+1;;
-//                 while(i<=nums.size()){
-//                     if(nums[i]==x){
-//                         i++;
-//                     }
-//                     else{
-//                         break;
-//                     }
-//                 }
-
-//                 if(i ==nums.size() && i== x){
-//                     return i;
-//                 }
-//                 else{
-//                     return i-1;
-    //             }
-                
-    //         } 
-    //         else if(nums[mid] > x){
-    //             high = mid - 1;
-    //         }
-    //         else low = mid + 1;
-    //     }
-    //     return -1;
-    // }
     vector<int> searchRange(vector<int>& nums, int target) {
-        
-        // int first = firstOcc(nums,target);
-        // // if(first == nums.size() || nums[first] != target) return {-1, -1};
-        // int last = lastOcc(nums,target);
-        // return {first,last};
-          int n = nums.size();
+        int n = nums.size();
         int low = 0;
         int high = n - 1;
         vector<int> ans;
@@ -65,17 +23,15 @@ public:
                  ans.push_back(j-1);
                  break;
 
-        }
-        else if(nums[mid]>target)high=mid-1;
-        else low=mid+1;
-       
-
+            }
+            else if(nums[mid]>target)high=mid-1;
+            else low=mid+1;
         
-    }
-    if(ans.size()==0){
-        ans.push_back(-1);
-        ans.push_back(-1);
-    }
+        }
+        if(ans.size()==0){
+            ans.push_back(-1);
+            ans.push_back(-1);
+        }
      
        
         return ans;
