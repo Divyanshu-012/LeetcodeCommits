3de512ab3f9968bd42f042d9fd8874ab88f1a1e5
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-08-29 00:59:25 +0530

    Time: 50 ms (71.95%), Space: 48.9 MB (70.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -3,36 +3,42 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (nums, target) {
-   let low = 0,
-        high = nums.length - 1;
+var search = function(nums, target) {
+    var low = 0, high  = nums.length-1;
 
-    while (low <= high) {
-        // Finding the mid using floor division
-        let mid = low + Math.floor((high - low) / 2);
+    while (low<=high){
+        let mid = low + Math.floor((high -low)/2);
 
-        // Target value is present at the middle of the
-        // array
-        if (nums[mid] == target) {
+        if(nums[mid]== target){
             return mid;
+
         }
 
-        // low to mid is sorted
-        if (nums[low] <= nums[mid]) {
-            if (nums[low] <= target && target < nums[mid]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        if(nums[low] <= nums[mid]){
+            if(nums[low] <= target && target < nums[mid]){
+                high = mid-1;
             }
-        }
-        // mid to high is sorted
-        else {
-            if (nums[mid] < target && target <= nums[high]) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+            else{
+                low= mid+1;
             }
+
         }
+        //mid<target
+        else{
+
+            if(nums[mid] < target && target <= nums[high]){
+                low = mid+1;
+                
+            }
+            else{
+                high = mid-1;
+            }
+
+
+        }
+        
+
     }
     return -1;
+    
 };
