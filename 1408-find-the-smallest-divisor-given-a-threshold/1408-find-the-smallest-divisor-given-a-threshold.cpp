8c7c31ebc91f45ff007b1a40a01adce88045a46f
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-09-02 02:16:09 +0530

    Time: 30 ms (70.36%), Space: 25 MB (58.43%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,25 +1,71 @@
 class Solution {
-private:
-    bool check(vector<int>& nums, int divisor, int threshold) {
-        int sum = 0;
-        for(auto&num : nums){
-            sum +=(num + divisor - 1) / divisor; //to divide num by divisor and round up the result.
-            if (sum > threshold) return false; // Early exit if sum exceeds threshold
-        }
-        return true;
-    }
 public:
-    int smallestDivisor(std::vector<int>& nums, int threshold) {
-        int left = 1;
-        int right = *max_element(nums.begin(), nums.end());
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if (check(nums, mid, threshold)){
-                right = mid - 1;
-            }else{
-                left = mid + 1;
-            }
+
+    int Max_Elm(vector<int>& nums){
+        int Maxi = INT_MIN;
+        for(int i = 0; i < nums.size();i++){
+            Maxi = max(nums[i],Maxi);
         }
-        return left;
+        return Maxi;
+    }
+
+    int calculateDivisor(vector<int>&nums, int mid){
+        int sum = 0;
+        for(int  i = 0; i< nums.size();i++){
+            sum += ceil((double)nums[i]/mid);
+        }
+        return sum;
+
+    }
+
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low = 1;
+        int high = Max_Elm(nums);
+
+        while(low<=high){
+            int mid = low + (high-low)/2 ;
+            int  sum = calculateDivisor(nums, mid);
+            if(sum <= threshold){
+                high  = mid-1;
+            }
+            else{
+                low= mid+1;
+            }
+
+        }
+        return low;
+        
     }
 };
+
+
+
+
+
+// class Solution {
+// private:
+    
+
+    
+
+//     bool check(vector<int>& nums, int divisor, int threshold) {
+//         int low = 1;
+//         int high = Max_Elm(nums);
+
+//         while(low<=high){
+//             int mid = low + (high-low)/2 ;
+//             int  sum = calculateDivisor(nums, mid);
+//             if(sum <= threshold){
+//                 high  = mid-1;
+//             }
+//             else{
+//                 low= mid+1;
+//             }
+
+//         }
+//         return low;
+
+        
+    
+//     }
+// };
