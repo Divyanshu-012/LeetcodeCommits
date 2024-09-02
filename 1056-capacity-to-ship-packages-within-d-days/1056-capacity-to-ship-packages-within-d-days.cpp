857f9cb7d8775a403b311f4437a51cdc68e3ddc2
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-09-03 03:36:07 +0530

    Time: 31 ms (94.28%), Space: 34 MB (10.67%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -37,20 +37,5 @@ public:
 }
 };
 
-int findDays(vector<int> &weights, int cap) {
-    int days = 1; //First day.
-    int load = 0;
-    int n = weights.size(); //size of array.
-    for (int i = 0; i < n; i++) {
-        if (load + weights[i] > cap) {
-            days += 1; //move to next day
-            load = weights[i]; //load the weight.
-        }
-        else {
-            //load the weight on the same day.
-            load += weights[i];
-        }
-    }
-    return days;
-}
+
 
