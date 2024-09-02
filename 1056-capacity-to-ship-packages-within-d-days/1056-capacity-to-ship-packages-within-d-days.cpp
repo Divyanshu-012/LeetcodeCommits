67dd8430df02acb6d26cbaa314079b46a5c9faee
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-09-03 03:35:47 +0530

    Time: 44 ms (26.24%), Space: 33.9 MB (10.67%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int findDays(vector<int> &weights, int cap) {
-    int days = 1; //First day.
+    int days = 1; 
     int load = 0;
-    int n = weights.size(); //size of array.
+    int n = weights.size();
     for (int i = 0; i < n; i++) {
         if (load + weights[i] > cap) {
-            days += 1; //move to next day
-            load = weights[i]; //load the weight.
+            days += 1; 
+            load = weights[i]; 
         }
         else {
-            //load the weight on the same day.
+            
             load += weights[i];
         }
     }
@@ -19,16 +19,17 @@ public:
     int shipWithinDays(vector<int> &weights, int cap) {
     //Find the maximum and the summation:
     int low = *max_element(weights.begin(), weights.end());
+    // stl function to find the sum of whole array in c++
     int high = accumulate(weights.begin(), weights.end(), 0);
     while (low <= high) {
         int mid = (low + high) / 2;
         int numberOfDays = findDays(weights, mid);
         if (numberOfDays <= cap) {
-            //eliminate right half
+            
             high = mid - 1;
         }
         else {
-            //eliminate left half
+            
             low = mid + 1;
         }
     }
