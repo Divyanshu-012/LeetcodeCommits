d9b7171e13c619b868bb44b77594cbd13c27925f
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-09-12 22:49:52 +0530

    Time: 0 ms (100%), Space: 7.7 MB (63.28%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
-    bool check(string word, char start,  char end){
-        for(char &ch : word){
-            if(ch < start || ch > end) return false;
+    // bool check(string word, char start,  char end){
+    //     for(char &ch : word){
+    //         if(ch < start || ch > end) return false;
             
-        }    
-        return true ;  
-    }
+    //     }    
+    //     return true ;  
+    // }
 
     
     bool detectCapitalUse(string word) {
-        if(check(word,'A','Z') || check(word,'a','z') || check(word.substr(1),'a','z')){
-            return true ;
-        }
-        return false ;
+    //     if(check(word,'A','Z') || check(word,'a','z') || check(word.substr(1),'a','z')){
+    //         return true ;
+    //     }
+    //     return false ;
+    // }
+
+    int countCap = 0;
+    for(char &ch :  word ){
+        if(isupper(ch)) 
+        countCap ++;
+
+    }
+
+    if(word.length() == countCap || countCap == 0 || (countCap == 1 && isupper(word[0]))) 
+    return true ;
+
+    return false ;
+
     }
 };
