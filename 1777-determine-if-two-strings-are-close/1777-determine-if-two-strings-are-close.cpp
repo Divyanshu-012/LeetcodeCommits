81dbef39c9355e615fcbf32043c3f1f1241fa51a
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-09-12 20:03:20 +0530

    Time: 59 ms (89.39%), Space: 22.2 MB (86.12%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
+
+        int m = word1.length();
+        int n = word2.length();
+        if(m!=n) return false;
+
+        
         vector<int> freq1(26, 0);
         vector<int> freq2(26, 0);
 
