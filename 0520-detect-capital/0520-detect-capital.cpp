b073ca51929e0991b8c54773ac7165f6eecd44f2
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-09-12 22:39:20 +0530

    Time: 4 ms (30.52%), Space: 8 MB (11.08%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    bool allCapital(string word){
+    bool check(string word, char start,  char end){
         for(char &ch : word){
-            if(ch <'A' || ch > 'Z') return false;
+            if(ch < start || ch > end) return false;
             
         }    
         return true ;  
     }
 
-    bool allSmall(string word){
-        for(char &ch : word){
-            if(ch <'a' || ch > 'z') return false;
-            
-        } 
-        return true ;     
-    }
+    
     bool detectCapitalUse(string word) {
-        if(allCapital(word) || allSmall(word) || allSmall(word.substr(1))){
+        if(check(word,'A','Z') || check(word,'a','z') || check(word.substr(1),'a','z')){
             return true ;
         }
         return false ;
