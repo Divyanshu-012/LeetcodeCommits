f7805a08804633363ace960e6245469ac3273451
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-09-24 22:14:02 +0530

    Time: 8 ms (44.34%), Space: 15.7 MB (69.26%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,26 +1,30 @@
+bool compare(int a, int b) {
+    string sa = to_string(a);
+    string sb = to_string(b);
+
+    // Custom comparison to check which concatenation is larger
+    if((sa + sb ) > (sb + sa)){
+        return true;
+    }
+    return false;
+}
+
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        // Convert the integers to strings
-        vector<string> strNums;
+        // Sort using the custom comparator
+        sort(nums.begin(), nums.end(), compare);
+
+        // If the largest number after sorting is '0', the result is "0"
+        if (nums[0] == 0) {
+            return "0";
+        }
+
+        string ans = "";
         for (int num : nums) {
-            strNums.push_back(to_string(num));
+            ans += to_string(num);
         }
 
-        // Custom sort comparator
-        sort(strNums.begin(), strNums.end(), [](const string &a, const string &b) {
-            return a + b > b + a;
-        });
-
-        // If the largest number is '0', then the entire number is '0'
-        if (strNums[0] == "0") return "0";
-
-        // Concatenate the sorted strings
-        string largestNum = "";
-        for (string s : strNums) {
-            largestNum += s;
-        }
-
-        return largestNum;
+        return ans;
     }
 };
