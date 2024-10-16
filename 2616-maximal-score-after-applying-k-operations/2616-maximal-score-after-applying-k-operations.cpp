644726fae4c234601cdca6d3e26379cd074e0424
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-10-16 20:04:07 +0530

    Time: 211 ms (13.2%), Space: 76 MB (75.3%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -1,27 +1,18 @@
+
 class Solution {
 public:
-    static long long maxKelements(vector<int>& nums, int k) {
-        priority_queue<int> pq(nums.begin(), nums.end());
-        long long score=0;
-        for(int i=0; i<k; i++){
-            int x=pq.top();
-            score+=x;
-            if (x==1){// early stop
-                score+=(k-1-i);
-                break;
-            }
-            pq.pop();
-            pq.push((x+2)/3);
+    long long maxKelements(std::vector<int>& nums, int k) {
+        long long sum = 0;
+        priority_queue<int> maxHeap(nums.begin(), nums.end());  
+        for (int i = 0; i < k; ++i) {
+            int max_val = maxHeap.top();  
+            maxHeap.pop();                
+            sum += max_val;               
+            
+            int new_val = ceil(max_val / 3.0);  
+            maxHeap.push(new_val);        
         }
-        return score;
+        
+        return sum;
     }
 };
-
-
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
