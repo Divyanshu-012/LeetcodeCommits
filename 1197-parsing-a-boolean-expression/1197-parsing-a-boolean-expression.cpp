259e43b2325c97c686cc3a3393aec98ef05d5034
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-10-21 02:02:32 +0530

    Time: 7 ms (64.18%), Space: 11.8 MB (31.73%) - LeetHub

--- 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
@@ -1,59 +1,66 @@
 class Solution {
 public:
 
-    char solve(vector<char> &values, char op){  // Change vector<int> to vector<char>
+    char solve(vector<char> &values, char op){
         if(op == '!'){
-            return values[0] == 't' ? 'f' : 't';  // Fix type comparison with 't' and 'f'
+            return values[0] == 't' ? 'f' : 't';
         }
 
         if(op == '&'){
             for(auto ch : values){
-                if(ch == 'f'){  // Check for 'f' correctly
+                if(ch == 'f'){
                     return 'f';
                 }
-            }
-            return 't';  // Return 't' if no 'f' is found
+                
+            }  
+            return 't';
+
         }
 
         if(op == '|'){
+
             for(auto ch : values){
-                if(ch == 't'){  // Check for 't' correctly
+                if(ch == 't'){
                     return 't';
                 }
-            }
-            return 'f';  // Return 'f' if no 't' is found
+                // return 'f';
+            } 
+            return 'f';
+
         }
 
-        return 't';  // Will never reach this point
+        return 't'; // will never come here 
     }
-
     bool parseBoolExpr(string expression) {
         int n = expression.length();
-        stack<char> st;
+        stack<char>st;
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i<n; i++){
             if(expression[i] == ',') 
                 continue;
-            
-            if(expression[i] == ')'){
+            if(expression[i]==')'){
                 vector<char> values;
 
                 while(st.top() != '('){
                     values.push_back(st.top());
                     st.pop();
+
+
                 }
 
-                st.pop(); // Remove '('
+                st.pop();
                 char op = st.top(); // !,&,|
                 st.pop();
 
-                st.push(solve(values, op));  // Push the result back onto the stack
+                st.push(solve(values,op));
 
-            } else {
-                st.push(expression[i]);  // Push the current character onto the stack
+            }   else{
+                st.push(expression[i]);
+
             } 
+            
         }
-
-        return st.top() == 't' ? true : false;  // Final evaluation
+        return st.top() == 't' ? true : false;
+        
     }
 };
