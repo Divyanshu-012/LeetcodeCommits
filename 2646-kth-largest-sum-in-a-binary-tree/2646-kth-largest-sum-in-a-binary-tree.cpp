63589e6f195e18138e74d6e13f2ad9d348137d7b
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-10-28 14:54:47 +0530

    Time: 27 ms (26.66%), Space: 162 MB (21.16%) - LeetHub

--- 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
@@ -11,34 +11,42 @@
  */
 class Solution {
 public:
-    long long kthLargestLevelSum(TreeNode* root, int k) {
-        vector<long long> res;      // To store sum of each level
-        queue<TreeNode*> q;         // Queue for level-order traversal (BFS)
-        
-        q.push(root);               // Start BFS from the root node
-        while (!q.empty()) {
-            int n = q.size();       // Number of nodes at the current level
-            long long sum = 0;      // Sum of node values at the current level
-            
-            // Process all nodes at the current level
-            while (n--) {
-                TreeNode* node = q.front(); q.pop();   // Get the front node from the queue
-                sum += (long long)node->val;           // Add node's value to the level sum
-                
-                // Push left and right children of the node to the queue (if they exist)
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-            res.push_back(sum);     // Store the sum of the current level
-        }
+    
+    void largestSum(TreeNode* root, vector<long long> &sum) {
+        queue<pair<TreeNode*, int>> q;  
+        q.push({root, 0});
 
-        // If k is greater than the number of levels, return -1
-        if (k > res.size()) return -1;
+        while(!q.empty()) {
+            int levelSize = q.size();  
+            long long levelSum = 0;
+
+            for(int i = 0; i < levelSize; i++) {
+                TreeNode* node = q.front().first;
+                int level = q.front().second;
+                q.pop();
+
+                levelSum += node->val; 
+
+                if(node->left != nullptr) {
+                    q.push({node->left, level + 1});
+                }
+                if(node->right != nullptr) {
+                    q.push({node->right, level + 1});
+                }
+            }
+            sum.push_back(levelSum);  
+        }
+    }
+
+    long long kthLargestLevelSum(TreeNode* root, int k) {
+        vector<long long> sum; 
+        largestSum(root, sum); 
+        sort(sum.begin(), sum.end());  
+
         
-        // Sort the level sums in descending order
-        sort(res.begin(), res.end(), greater<long long>());
-        
-        // Return the k-th largest level sum (k-1 due to 0-based indexing)
-        return res[k-1];
+        if(k > sum.size()) return -1;
+
+        int ans = sum.size() - k ;
+        return sum[ans];  
     }
 };
