3d9768c8d8bd4fcdff7eae487ec00794d20e9529
Divyanshu Kapariya <kapariyadiv@gmail.com>
2024-11-15 02:40:20 +0530

    Time: 63 ms (42.35%), Space: 15 MB (37.5%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -4,6 +4,7 @@ public:
         int n = nums.size();
         vector<int> LIS(n, 1), LDS(n, 1);
 
+//LIS
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
                 if (nums[i] > nums[j]) {
@@ -12,6 +13,7 @@ public:
             }
         }
 
+//LDS
         for (int i = n - 1; i >= 0; --i) {
             for (int j = n - 1; j > i; --j) {
                 if (nums[i] > nums[j]) {
