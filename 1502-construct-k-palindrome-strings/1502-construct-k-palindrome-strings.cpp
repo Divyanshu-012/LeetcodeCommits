04712316ec8b3a7ad795a2007053b00b56ebfe73
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-01-11 17:33:54 +0530

    Time: 18 ms (20.98%), Space: 15 MB (33.92%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        unordered_map<char,int> mp;
+
+        if (s.size() < k) return false;
+
+        for(auto ch: s){
+            mp[ch]++;
+        }
+
+        int cntO = 0, cntE = 0;
+        for(auto it : mp){
+            if((it.second % 2 ) != 0) cntO++;
+            else cntE++;
+
+        }
+
+        if (cntO> k) return false;
+        if (cntO ==  k) return true; 
+        if (cntO < k ) return true; 
+
+        return true ;
+    }
+};
