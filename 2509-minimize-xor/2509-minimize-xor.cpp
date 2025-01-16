d3f0d37551a618281c41f8cc8031320d0caf678a
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-01-16 19:23:40 +0530

    Time: 0 ms (100%), Space: 7.9 MB (86.18%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.cpp
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -1,47 +1,42 @@
 class Solution {
 public:
-    int minimizeXor(int num1, int num2) {
-        
-        int result = num1;
-
-        int targetSetBitsCount = __builtin_popcount(num2);
-        int setBitsCount = __builtin_popcount(result);
-
-        
-        int currentBit = 0;
-
-        
-        while (setBitsCount < targetSetBitsCount) {
-            
-            if (!isSet(result, currentBit)) {
-                setBit(result, currentBit);
-                setBitsCount++;
-            }
-            
-            currentBit++;
-        }
-
-        
-        while (setBitsCount > targetSetBitsCount) {
-            
-            if (isSet(result, currentBit)) {
-                unsetBit(result, currentBit);
-                setBitsCount--;
-            }
-            
-            currentBit++;
-        }
-
-        return result;
+    bool isSet(int &x , int bit){
+        return x & (1<< bit);
     }
 
-private:
-    // Helper function to check if the given bit position in x is set (1).
-    bool isSet(int x, int bit) { return x & (1 << bit); }
+    bool setBit(int &x, int bit){
+        return x |= (1<<bit);
+    }
 
-    // Helper function to set the given bit position in x to 1.
-    void setBit(int &x, int bit) { x |= (1 << bit); }
+    bool unsetBit(int &x, int bit){
+       return  x &= ~(1 << bit);
+    }
+    int minimizeXor(int num1, int num2) {
+        int x= num1;
 
-    // Helper function to unset the given bit position in x (set it to 0).
-    void unsetBit(int &x, int bit) { x &= ~(1 << bit); }
+        int reqSetBitCnt = __builtin_popcount(num2); // function to count set bits 
+        int currSetBitCnt = __builtin_popcount(x);
+    int bit = 0; // position of bit
+        if(currSetBitCnt < reqSetBitCnt) {
+            while(currSetBitCnt < reqSetBitCnt){
+                if(!isSet(x,bit)){
+                    setBit(x,bit);
+                    currSetBitCnt++;
+                }
+                bit++;
+            }
+        }
+        else if(currSetBitCnt > reqSetBitCnt){
+            while(currSetBitCnt > reqSetBitCnt){
+                if(isSet(x,bit)){
+                    unsetBit(x,bit);
+                    currSetBitCnt--;
+                }
+                bit++;
+            }
+
+        }
+        return x;
+        
+    }
 };
