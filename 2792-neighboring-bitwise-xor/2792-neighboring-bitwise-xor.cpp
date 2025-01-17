cfb93fd4364899f048ced178527de6de1d0c42b6
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-01-17 16:09:58 +0530

    Time: 60 ms (5.14%), Space: 302.6 MB (5.61%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,30 +1,60 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& d) {
-        if(d.size() == 1) return d[0] == 0;
+        if (d.size() == 1) return d[0] == 0;
         
-        vector<int> ans;
         int n = d.size();
-        
-        
-        if(d[0] == 1) {
-            ans.push_back(0);
-            ans.push_back(1);
-        } else {
-            ans.push_back(0);
-            ans.push_back(0);
-        }
-        
-        // Build the ans vector
-        for(int i = 1; i < n; i++) {
-            if(ans.back() == 1) {
-                ans.push_back(d[i] == 1 ? 0 : 1);
+        vector<int> ans;
+        queue<int> q;
+
+        if (n > 1) {
+            for (int i = 0; i < d.size()-1; i++) {
+                if (i == 0) {
+                    // Initialize the vector and queue
+                    if (d[i] == 1) {
+                        ans.push_back(0);
+                        ans.push_back(1);
+                        q.push(1);
+                    } else {
+                        ans.push_back(0);
+                        ans.push_back(0);
+                        q.push(0);
+                    }
+                } else {
+                    // Make use of the queue to build the vector
+                    if (q.front() == 1) {
+                        if (d[i] == 1) {
+                            ans.push_back(0);
+                            q.pop();
+                            q.push(0);
+                        } else {
+                            ans.push_back(1);
+                            q.pop();
+                            q.push(1);
+                        }
+                    } else {
+                        if (d[i] == 1) {
+                            ans.push_back(1);
+                            q.pop();
+                            q.push(1);
+                        } else {
+                            ans.push_back(0);
+                            q.pop();
+                            q.push(0);
+                        }
+                    }
+                }
+            }
+
+            int m = ans.size();
+            // Check if the last element in d matches the XOR of the last and first elements in ans
+            if (d[n-1] == (ans[m-1] ^ ans[0])) {
+                return true;
             } else {
-                ans.push_back(d[i] == 1 ? 1 : 0);
+                return false;
             }
         }
-        
-      
-        return d[n-1] == (ans[n-1] ^ ans[0]);
+
+        return false;
     }
 };
