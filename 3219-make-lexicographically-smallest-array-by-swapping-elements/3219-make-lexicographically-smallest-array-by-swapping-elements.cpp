c51f90cb993ef7e522fdda0cb16e6650f0315602
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-01-26 17:06:32 +0530

    Time: 342 ms (29.05%), Space: 234.2 MB (25%) - LeetHub

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -5,20 +5,20 @@ public:
         vector<int>sor = nums;
         
 
-        sort(sor.begin(),sor.end());
+        sort(sor.begin(),sor.end()); // O(nlogn)
         int groupNum = 0;
 
         // this map is for --> konsa no kis group mei gya 
-        unordered_map<int,int> numToGroup;
+        unordered_map<int,int> numToGroup; // O(1)
         numToGroup[sor[0]] = groupNum;
 
         // this map is for --> kis group mei kon kon sa elment hai 
 
         unordered_map<int,list<int>> groupToList;  
         // list is used in above map to make it a doubly linked list , because to pop the first item it is useful as in vector to pop the front element it is task of O(n), but in linked list it is of O(1) only 
-        groupToList[groupNum].push_back(sor[0]);
+        groupToList[groupNum].push_back(sor[0]); // O(1)
     
-        for(int  i =1; i< n; i++){
+        for(int  i =1; i< n; i++){ //O(n)
             if(abs(sor[i]-sor[i-1]) > lim){
                 groupNum +=1;
 
@@ -29,7 +29,7 @@ public:
         }
 
         // build the answer !!
-        for(int i = 0 ; i< n ; i++){
+        for(int i = 0 ; i< n ; i++){  //O(n)
             int num  = nums[i];
             int group = numToGroup[num];
 
