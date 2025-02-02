28c3cde2a8f3ad9557acc05b9145ef8f999dd3eb
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-02-02 17:39:15 +0530

    Time: 3 ms (77.51%), Space: 10.8 MB (92.81%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     string convert(string s, int r) {
-        if (r == 1) return s; // No zigzag pattern needed
+        if (r == 1) return s; 
 
         int n = s.size();
         string ans;
-        int d = 2 * r - 2;
+        int d = 2 * (r - 1);
 
         for (int i = 0; i < r; i++) {
             for (int j = 0; j + i < n; j += d) {
