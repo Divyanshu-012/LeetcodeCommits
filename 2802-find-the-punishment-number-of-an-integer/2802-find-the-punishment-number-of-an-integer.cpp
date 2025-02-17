8b121d49632e7b675945db0c8d4da27dd1bd64ee
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-02-17 23:32:08 +0530

    Time: 280 ms (28.15%), Space: 11.2 MB (36.6%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -10,16 +10,17 @@ public:
             return false;
         }
 
-        bool possible = false;
+        // bool possible = false;
+
         for(int j = i ; j< s.length(); j++){
             string sub = s.substr(i, j-i+1);
             int val = stoi(sub);
 
-            possible = possible || check(j+1, currsum+val, s, num);
+            if(check(j+1, currsum+val, s, num)== true) return true;
 
-            if(possible == true){
-                return true ;
-            }
+            // if(possible == true){
+            //     return true ;
+            // }
         }
         return false;
     }
