5230510104fdef215948a22d28f5786f219a2595
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-03-04 17:47:21 +0530

    Time: 0 ms (100%), Space: 14.7 MB (65.2%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1,
-                                    vector<vector<int>>& nums2) {
-        map<int, int> keyToSum;
-
-        // Copying the array nums1 to the map.
-        for (auto nums : nums1) {
-            keyToSum[nums[0]] = nums[1];
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        
+        int i = 0, j = 0;
+        vector<vector<int>> result;
+        
+        
+        while (i < nums1.size() && j < nums2.size()) {
+            int id1 = nums1[i][0], val1 = nums1[i][1];
+            int id2 = nums2[j][0], val2 = nums2[j][1];
+            
+            if (id1 < id2) {
+                
+                result.push_back({id1, val1});
+                i++;
+            } else if (id2 < id1) {
+                
+                result.push_back({id2, val2});
+                j++;
+            } else {
+                
+                result.push_back({id1, val1 + val2});
+                i++;
+                j++;
+            }
         }
-
-        // Adding the values to existing keys or create new entries.
-        for (auto nums : nums2) {
-            keyToSum[nums[0]] += nums[1];
+        
+       
+        while (i < nums1.size()) {
+            result.push_back(nums1[i]);
+            i++;
         }
-
-        vector<vector<int>> mergedArray;
-        for (auto it : keyToSum) {
-            mergedArray.push_back({it.first, it.second});
+        
+        
+        while (j < nums2.size()) {
+            result.push_back(nums2[j]);
+            j++;
         }
-
-        return mergedArray;
+        
+        return result;
     }
 };
