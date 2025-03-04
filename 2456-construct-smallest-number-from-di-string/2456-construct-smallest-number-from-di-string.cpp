b0adbe8c09dc08a8776458ccefc22852c0afac64
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-03-04 18:54:57 +0530

    Time: 19 ms (16.45%), Space: 7.6 MB (99.57%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    string smallestNumber(string pattern) {
-        return to_string(findSmallestNumber(pattern, 0, 0, 0));
-    }
-
-private:
-    // Recursively find the smallest number that satisfies the pattern
-    int findSmallestNumber(string pattern, int currentPosition,
-                           int usedDigitsMask, int currentNum) {
-        // Base case: return the current number when the whole pattern is
-        // processed
-        if (currentPosition > pattern.size()) return currentNum;
-
-        int result = INT_MAX;
-        int lastDigit = currentNum % 10;
-        bool shouldIncrement =
-            currentPosition == 0 || pattern[currentPosition - 1] == 'I';
-
-        // Try all possible digits (1 to 9) that are not yet used and follow the
-        // pattern
-        for (int currentDigit = 1; currentDigit <= 9; ++currentDigit) {
-            if ((usedDigitsMask & 1 << currentDigit) == 0 &&
-                currentDigit > lastDigit == shouldIncrement)
-                result = min(result, findSmallestNumber(
-                                         pattern, currentPosition + 1,
-                                         usedDigitsMask | 1 << currentDigit,
-                                         currentNum * 10 + currentDigit));
+    bool matchesP(string &num , string &p){
+        for(int i = 0 ; i< p.length(); i++){
+            if( (p[i] == 'I' && num[i] > num[i+1] ) || (p[i] == 'D' && num[i] < num[i+1])){
+                return false;
+            }
         }
 
-        return result;
+        return true;
+    }
+    string smallestNumber(string p) {
+        int n = p.length();
+        string num = "";
+
+        for(int i = 1; i<= n+1; i++){
+            num.push_back( i +'0');
+        }
+
+        while(!matchesP(num,p)){
+            next_permutation(begin(num),end(num));
+        }
+
+        return num;
     }
 };
