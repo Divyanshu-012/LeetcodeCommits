da92efb928464039246614964a3279a6dd96c372
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-05-14 00:11:20 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,11 +3,11 @@ public:
     int climbStairs(int n) {
         int prev2 = 1;
         int prev = 1;
-        if(n <= 2 ) return n;
+        
         for(int i = 2 ; i<= n ; i++){
-            int curr_i = prev + prev2;
+            int curr = prev + prev2;
             prev2 = prev;
-            prev = curr_i;
+            prev = curr;
         }
         return prev;
         
