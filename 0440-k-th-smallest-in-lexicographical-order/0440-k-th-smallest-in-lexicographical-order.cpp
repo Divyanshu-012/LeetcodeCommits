3aa5c24762f98d232d64847a56ce1e81914a98d0
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-06-11 01:37:47 +0530

    Time: 0 ms (100%), Space: 7.7 MB (99.62%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-    int Count(long curr, long next, int n) {
+    int Count(long curr,long next,int n){
         int cnt = 0;
-        while (curr <= n) {
-            cnt += min((long)n + 1, next) - curr;
+
+        while(curr <=n){
+            cnt +=  next - curr;
             curr *= 10;
-            next *= 10;
+            next*=10;
+
+            if(next>n) next = min(next,long(n+1));
+             
+            
+                
+            
+            
+            
         }
         return cnt;
-    }
 
+    }
     int findKthNumber(int n, int k) {
         int curr = 1;
-        k--; // we consider 1 as already included
+        k=k-1; // as we already considering 1 as the 1st nunber , so k-1
 
-        while (k > 0) {
-            int cnt = Count(curr, curr + 1, n);
-            if (cnt <= k) {
-                curr += 1;
-                k -= cnt;
-            } else {
-                curr *= 10;
+        while(k > 0){
+            int cnt = Count(curr,curr+1,n);
+            if(cnt <= k){// skip the current tree
+                curr+=1;
+                k-=cnt;
+            }else{
+                curr*=10;
                 k--;
             }
         }
-
         return curr;
     }
 };
