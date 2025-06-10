a0219091e8a29a17f4aae1c10c6eaa938c07b89d
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-06-10 17:26:08 +0530

    Time: 1022 ms (12.64%), Space: 346 MB (7.18%) - LeetHub

--- 3892-best-time-to-buy-and-sell-stock-v/3892-best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/3892-best-time-to-buy-and-sell-stock-v.cpp
@@ -1,15 +1,46 @@
 class Solution {
 public:
-        long long maximumProfit(vector<int>& A, int k) {
-        vector<long long> res(k + 1), bought(k, -1e9), sold(k);
-        for (int a : A) {
-            for (int j = k; j >= 1; --j) {
-                res[j] = max({res[j], bought[j - 1] + a, sold[j - 1] - a});
-                bought[j - 1] = max(bought[j - 1], res[j - 1] - a);
-                sold[j - 1] = max(sold[j - 1], res[j - 1] + a);
+    vector<int> prices;
+    long long mn = -1e14;
+    vector<vector<vector<long long>>> dp;
+    long long f(int i, int k, int state){
+        if(i == prices.size()){
+            return state == 0 ? 0 : mn;
+        }
+        if(dp[i][k][state] != mn) return dp[i][k][state];
+
+        long long p = prices[i];
+        long long profit = mn;
+        // 1) do nothing today
+        profit = max(profit, f(i + 1, k, state));
+
+        // 2) If neither sold nor bought earlier
+        if(state == 0){
+            // start 2 type of transaction
+            // a) buy, losing p
+            profit = max(profit, f(i+1, k, 1) - p);
+            // b) sell, gaining p
+            profit = max(profit, f(i+1, k, 2) + p);
+        }
+        else if(k > 0){ // have transactions left
+            // complete 1 transaction
+            if(state == 1){
+                // already bought then sell incurring profit of p
+                profit = max(profit, f(i+1, k-1, 0) + p);
+                
+            }
+            else{
+                // already sold then buy incurring loss of p
+                 profit = max(profit, f(i+1, k-1, 0) - p);
             }
         }
-        return *max_element(res.begin(), res.end());
+        return dp[i][k][state] = profit;
     }
-
+    
+    long long maximumProfit(vector<int>& prices, int k) {
+        this->prices = prices;
+        
+        dp.assign(prices.size() + 1, vector<vector<long long>> (k + 1, vector<long long> (3, mn)));
+        return f(0, k, 0);  
+    }
 };
