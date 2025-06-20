fc9590271e4abd74dd132ac67d7a0dbefd5c78c4
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-06-20 17:01:08 +0530

    Time: 0 ms (100%), Space: 12.1 MB (85.41%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        sort(c.begin(),c.end());
+
+        int ans = 0;
+        int cnt = 0;
+        int n = c.size();
+        
+
+        // incase i got  FIX number 
+        for(int i =0; i<n;i++){
+            int fix = c[i];
+            cnt = n-i;
+            if(cnt >= fix) ans = max(ans,fix);
+            if(fix > cnt){
+                ans = max(cnt,ans);
+            } 
+        }
+
+        if(ans == 0){
+            if(c[n-1]==0)return 0; //true always
+            
+        }
+        return ans;
+    }
+};
