7259b5c69385a1c55b0b628446b9e2ab02f4b5bc
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-07-18 22:27:26 +0530

    Time: 4 ms (24.56%), Space: 9.4 MB (57.63%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -8,13 +8,13 @@ public:
             char ch1 = s[i];
             char ch2 = t[i];
 
-            if(m1.find(ch1) != m1.end()){ // Case 1: ch1 already mapped in m1
-                if(ch2 != m1[ch1]){       // Check if ch1 maps to the correct character
+            if(m1.find(ch1) != m1.end()){ 
+                if(ch2 != m1[ch1]){      
                     return false;
                 }
-            }else{                        // Case 2: ch1 is not yet mapped in m1
-                if(m2.find(ch2) != m2.end()){  // Check if ch2 is already mapped to some character
-                    return false;         // If so, strings aren't isomorphic
+            }else{                        
+                if(m2.find(ch2) != m2.end()){  
+                    return false;       
                 }
             }
   
