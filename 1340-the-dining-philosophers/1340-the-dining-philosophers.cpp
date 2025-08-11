154352d42f5a0d9e058d15c0325b576a309f41db
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-08-11 20:26:45 +0530

    Time: 33 ms (50.88%), Space: 18.7 MB (30.13%) - LeetHub

--- 1340-the-dining-philosophers/1340-the-dining-philosophers.cpp
+++ 1340-the-dining-philosophers/1340-the-dining-philosophers.cpp
@@ -1,61 +1,36 @@
-class Semaphore {
-    public:
-    Semaphore() {}
-    Semaphore(int c) : count(c){};
-
-    void setCount(int a){
-        count = a;
-
-    }
-    inline void signal(){
-        unique_lock<mutex>lock(mtx);
-        count++;
-        if(count <= 0)cv.notify_one();
-    }
-    inline void wait(){
-        unique_lock<mutex>lock(mtx);
-        count--;
-        while(count < 0){
-            cv.wait(lock);
-        }
-    }
-    private:
-    mutex mtx;
-    condition_variable cv;
-    int count;
-
-};
 
 class DiningPhilosophers {
-    Semaphore fork[5];
-    mutex m,l;
-public:
-    DiningPhilosophers() {
-        for(int i = 0;i<5;i++){
-            fork[i].setCount(1);
-        }
-    }
+    mutex forks[5];  // one mutex per fork
 
+public:
     void wantsToEat(int philosopher,
                     function<void()> pickLeftFork,
                     function<void()> pickRightFork,
                     function<void()> eat,
                     function<void()> putLeftFork,
-                    function<void()> putRightFork) {
-                        {
-               lock_guard<mutex>lock(m);
-               fork[(philosopher + 1)% 5].wait();
-               fork[philosopher].wait();         
-		
+                    function<void()> putRightFork) 
+    {
+        int leftFork = philosopher;           // fork on philosopher's left
+        int rightFork = (philosopher + 1) % 5; // fork on right
+
+        // Lock in a fixed order to avoid deadlock
+        if (philosopher % 2 == 0) {
+            forks[leftFork].lock();
+            forks[rightFork].lock();
+        } else {
+            forks[rightFork].lock();
+            forks[leftFork].lock();
+        }
+
+        // Actions
+        pickLeftFork();
+        pickRightFork();
+        eat();
+        putLeftFork();
+        putRightFork();
+
+        // Unlock both forks
+        forks[leftFork].unlock();
+        forks[rightFork].unlock();
     }
-    pickLeftFork();
-    pickRightFork();
-
-    eat();
-
-    putLeftFork();
-    fork[(philosopher +1) % 5].signal();
-    putRightFork();
-    fork[philosopher].signal();
-}
 };
