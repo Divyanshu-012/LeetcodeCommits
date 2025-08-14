399895a64a17aa22bd7dee1916b348cea6779ced
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-08-14 22:24:27 +0530

    Time: 123 ms (55.4%), Space: 198.4 MB (25.37%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -1,54 +1,71 @@
 class LFUCache {
-    int capacity;
-    int minFreq;
-    unordered_map<int,pair<int,int>> keyVal;
-    unordered_map<int,list<int>> freqList;
-    unordered_map<int,list<int>::iterator> pos;
 public:
+    int cap;
+    int size ;
+
+    unordered_map<int,list<vector<int>>::iterator>mp; // key -> Address
+
+    map<int,list<vector<int>>>freq; //counter -> {key,val,count}
     LFUCache(int capacity) {
-        this->capacity = capacity;
-        minFreq = 0;
+        cap = capacity;
+        size = 0;
+
+    }
+
+    void makeMostFU(int key){
+        auto &vec = *(mp[key]);
+
+        int value = vec[1];
+        int f = vec[2];
+
+        freq[f].erase(mp[key]);
+
+        if(freq[f].empty())
+           freq.erase(f);
+
+        f++;
+        freq[f].push_front({key,value,f});
+
+        mp[key]= freq[f].begin();
+
     }
     
     int get(int key) {
-        if(keyVal.find(key) == keyVal.end())
-            return -1;
-        freqList[keyVal[key].second].erase(pos[key]);
-        keyVal[key].second++;
-        freqList[keyVal[key].second].push_back(key);
-        pos[key] = --freqList[keyVal[key].second].end();
-        if(freqList[minFreq].empty())
-            minFreq++;
-        return keyVal[key].first;
+        if(mp.find(key)==mp.end())return -1;
+
+        auto vec = *(mp[key]);
+        int value = vec[1];
+
+        makeMostFU(key);
+        return value;
+
     }
     
     void put(int key, int value) {
-        if(!capacity)
-            return;
-        if(keyVal.find(key) != keyVal.end()) {
-            keyVal[key].first = value;
-            freqList[keyVal[key].second].erase(pos[key]);
-            keyVal[key].second++;
-            freqList[keyVal[key].second].push_back(key);
-            pos[key] = --freqList[keyVal[key].second].end();
-            if(freqList[minFreq].empty())
-                minFreq++;
-            return;
+    if(cap == 0) return;
+
+    if(mp.find(key) != mp.end()) {
+        auto &vec = *(mp[key]);
+        vec[1] = value;
+        makeMostFU(key);
+    } else {
+        if(size == cap) {
+            // Evict least frequently used
+            auto &lfu_list = freq.begin()->second;  // use reference
+            int key_to_delete = lfu_list.back()[0];
+            lfu_list.pop_back();
+            if(lfu_list.empty()) freq.erase(freq.begin()->first);
+            mp.erase(key_to_delete);
+            size--;
         }
-        if(keyVal.size() == capacity) {
-            int delKey = freqList[minFreq].front();
-            keyVal.erase(delKey);
-            pos.erase(delKey);
-            freqList[minFreq].pop_front();
-        }
-        keyVal[key] = {value,1};
-        freqList[1].push_back(key);
-        pos[key] = --freqList[1].end();
-        minFreq = 1;
+        // Insert new key
+        freq[1].push_front({key,value,1});
+        mp[key] = freq[1].begin();
+        size++;
     }
+}
+
 };
-
-
 
 /**
  * Your LFUCache object will be instantiated and called as such:
