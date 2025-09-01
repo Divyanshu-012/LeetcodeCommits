0d1cc6e5ea306a8532af32bae09d8415db654366
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-09-01 23:11:03 +0530

    Time: 283 ms (77.49%), Space: 97.7 MB (88.3%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
+    #define P pair<double, int>
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        int n = classes.size();
+
+        priority_queue<P> pq; //max-heap  -  //{max-delta, idx}
+
+        for(int i = 0; i < n; i++) {
+            double current_PR = (double)classes[i][0]/classes[i][1];
+            double new_PR = (double)(classes[i][0]+1)/(classes[i][1]+1);
+            double delta = new_PR - current_PR;
+            pq.push({delta, i});
+        }
         
-        auto gain = [&](int pass, int total){
-            return (double)(pass+1)/(total+1) - (double)pass/total;
-        };
-
-        priority_queue<pair<double,pair<int,int>>> pq;
-
-        for(int i=0;i<classes.size();i++){
-            int pass = classes[i][0];
-            int total = classes[i][1];
-            pq.push({gain(pass,total),{pass,total}});
-        }
-
-        while(extraStudents--){
-            auto it = pq.top();
+        //O(extraStudents * log(n))
+        while(extraStudents--) { //O(k)
+            auto curr = pq.top(); //log(n)
             pq.pop();
-            int pass = it.second.first;
-            int total = it.second.second;
-            pass++;
-            total++;
-            pq.push({gain(pass,total),{pass,total}});
+
+            double delta = curr.first;
+            int idx = curr.second;
+
+            classes[idx][0]++; //incremeent total passing students in the class
+            classes[idx][1]++; //increment total students oin the class
+
+            double current_PR = (double)classes[idx][0]/classes[idx][1];
+            double new_PR = (double)(classes[idx][0]+1)/(classes[idx][1]+1);
+            delta = new_PR - current_PR;
+            pq.push({delta, idx}); //log(n)
         }
 
-        double ans = 0;
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
-            ans += (double)it.second.first/it.second.second;
+        double result = 0.0;
+        for(int i = 0; i < n; i++) {
+            result += (double)classes[i][0]/classes[i][1];
         }
 
-        return ans/(double)classes.size();
+        return result/n;
     }
 };
