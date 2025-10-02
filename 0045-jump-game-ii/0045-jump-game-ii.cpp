e8ef58e0fa8ab9a629f2aeb0d4333f395e77fd91
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-10-02 10:26:57 +0530

    Time: 223 ms (6.32%), Space: 22.7 MB (5.63%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int near = 0, far = 0, jumps = 0;
+    int fn(int idx,vector<int>& nums,vector<int>&dp){
+        int n = nums.size();
+        if(idx >= n-1)return 0;
 
-        while (far < nums.size() - 1) {
-            int farthest = 0;
-            for (int i = near; i <= far; i++) {
-                farthest = max(farthest, i + nums[i]);
+        if(dp[idx] != -1)return dp[idx];
+        int mini = INT_MAX;
+
+        for(int i = 1; i<= nums[idx];i++){
+            int next = fn(idx + i, nums, dp);
+            if (next != INT_MAX) {
+                mini = min(mini, 1 + next); 
             }
-            near = far + 1;
-            far = farthest;
-            jumps++;
         }
-
-        return jumps;        
+        return dp[idx] = mini;
+    }
+    int jump(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+       return fn(0,nums,dp);
     }
 };
