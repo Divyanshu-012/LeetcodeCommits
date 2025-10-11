36bdcad5b6cab20f1309bf89d859d87925c26a39
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-10-12 03:39:11 +0530

    Time: 420 ms (31.48%), Space: 250.3 MB (14.09%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
@@ -1,45 +1,40 @@
 class Solution {
 public:
-    long long dp[100001];
-    long long fun(vector<int>&v , map<int,int>&m , int in){
-        if(in >= v.size()){
+    typedef long long ll;
+    int n;                                
+    unordered_map<ll,ll> mp;      
+    vector<ll> t;           
+
+    long long solve(int i, const vector<ll>& nums) {
+        if (i >= n) 
             return 0;
-        }
 
-        if(dp[in] != -1){
-            return dp[in];
-        }
+        if (t[i] != -1)
+            return t[i];
 
-        long long ans = 0 ;
+        //skip current damage
+        ll skip = solve(i + 1, nums);
 
-        auto it = upper_bound(v.begin() , v.end() , v[in] + 2);
+        //take current damage
+        int j = lower_bound(begin(nums) + i + 1, end(nums), nums[i] + 3) - begin(nums);
+        ll take = nums[i] * mp[nums[i]] + solve(j, nums);
 
-        if(it != v.end()){
-            ans = max(ans , (long long)v[in] * (long long)m[v[in]] + fun(v , m , it - v.begin()));
-        }
-        else{
-            ans = max(ans , (long long)v[in] * (long long)m[v[in]] );
-        }
-
-        ans = max(ans , fun(v , m , in+1));
-
-        return dp[in] = ans;
+        return t[i] = max(skip, take);
     }
+
     long long maximumTotalDamage(vector<int>& power) {
-        memset(dp , -1 , sizeof(dp));
-        map<int,int>m;
-        long long ans = 0 ;
+        for (int x : power) 
+            mp[x]++;
 
-        for(auto p : power){
-            m[p]++;
-        }
+        vector<ll> nums(mp.size());
 
-        vector<int>v;
+        for (auto &p : mp)
+            nums.push_back(p.first);
 
-        for(auto e : m){
-            v.push_back(e.first);
-        }
+        sort(begin(nums), end(nums));
+        n = nums.size();
+        t.assign(n, -1);
 
-        return fun(v , m , 0);
+        return solve(0, nums);
     }
 };
