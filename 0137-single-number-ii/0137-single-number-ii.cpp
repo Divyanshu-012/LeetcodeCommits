2b376764dada5f610e49d145c48d2f637da01d83
Divyanshu Kapariya <kapariyadiv@gmail.com>
2025-11-18 05:11:45 +0530

    Time: 0 ms (100%), Space: 13.5 MB (29.9%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for (int i=0; i<32; i++) {
+            int count = 0;
+            int mask = 1<<i;
+            for(int num: nums) {
+                if(num & mask)  count++;
+            }
+            if (count%3) res = res|mask;
+        }
+        return res;
+    }
+};
