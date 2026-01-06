1998d91e7c31f2f5f4a09ba08b6062189bef47e7
Divyanshu Kapariya <kapariyadiv@gmail.com>
2026-01-06 21:33:00 +0530

    Time: 2695 ms (5.1%), Space: 549.9 MB (5.2%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
+    vector<vector<vector<int>>> dp;
+
+    vector<int> fn(int idx, int prev, vector<int>& nums) {
+        int n = nums.size();
+        if (idx == n) return {};
+
+        if (!dp[idx][prev + 1].empty())
+            return dp[idx][prev + 1];
+
+        // skip 
+        vector<int> skip = fn(idx + 1, prev, nums);
+
+        // take 
+        vector<int> take;
+        if (prev == -1 || nums[idx] % nums[prev] == 0) {
+            take = fn(idx + 1, idx, nums);
+            take.insert(take.begin(), nums[idx]);
+        }
+
+        // Choose larger subset
+        if (take.size() > skip.size())
+            dp[idx][prev + 1] = take;
+        else
+            dp[idx][prev + 1] = skip;
+
+        return dp[idx][prev + 1];
+    }
+
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<int> dp(nums.size(), 1), prev(nums.size(), -1);
-        int maxi = 0;
-        for (int i = 1; i < nums.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                    prev[i] = j;
-                }
-            }
-            if (dp[i] > dp[maxi]) maxi = i;
-        }
-        vector<int> res;
-        for (int i = maxi; i >= 0; i = prev[i]) {
-            res.push_back(nums[i]);
-        }
-        return res;
+        sort(nums.begin(), nums.end());  
+        int n = nums.size();
+        dp.resize(n, vector<vector<int>>(n + 1));
+
+        return fn(0, -1, nums);
     }
 };
